46e0f564774e6d813192e07c2dc7fe1dda199a87
nyuusen <pppike.0131@gmail.com>
2022-12-20 22:09:16 +0900

    課題2のUnitTest実装

--- test/01.jest/jestSample/__tests__/functions.test.ts
+++ test/01.jest/jestSample/__tests__/functions.test.ts
@@ -1,9 +1,95 @@
 import * as func from '../functions'
+import { NameApiService } from '../nameApiService';
+import { DatabaseMock } from '../util'
 
-test('sumOfArrayが正しい計算結果を返す', () => {
-  expect(func.sumOfArray([1,2,3])).toBe(6);
+jest.mock('../util');
+jest.mock('../nameApiService')
+
+describe('課題2', () => {
+  describe('sumOfArray', () => {
+    it('[1]を渡すと1が返却される', () => {
+      expect(func.sumOfArray([1])).toBe(1);
+    })
+    it('[1,2,3]を渡すと6が返却される', () => {
+      expect(func.sumOfArray([1,2,3])).toBe(6);
+    })
+    it('空配列[]を渡すと例外が投げられる', () => {
+      expect(() => func.sumOfArray([])).toThrow();
+    })
+  })
+  describe('asyncSumOfArray', () => {
+    it('[1]を渡すと1が返却される', () => {
+      expect(func.asyncSumOfArray([1])).resolves.toBe(1);
+    })
+    it('[1,2,3]を渡すと6が返却される', () => {
+      expect(func.asyncSumOfArray([1,2,3])).resolves.toBe(6);
+    })
+    it('空配列[]を渡すと例外が投げられる', () => {
+      expect(() => func.asyncSumOfArray([])).rejects.toThrow();
+    })
+  })
+  describe('asyncSumOfArraySometimesZero', () => {
+    const mockedDb = new DatabaseMock();
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    })
+    describe('saveが正常に処理される', () => {
+      jest.spyOn(mockedDb, 'save').mockImplementation(() => {});
+      it('[1]を渡すと1が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([1], mockedDb)).resolves.toBe(1)
+      })
+      it('[1,2,3]を渡すと6が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([1,2,3], mockedDb)).resolves.toBe(6)
+      })
+      it('[]を渡すと0が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([], mockedDb)).resolves.toBe(0)
+      })
+    })
+    describe('saveが例外をthrowする', () => {
+      jest.spyOn(mockedDb, 'save').mockImplementation(() => {throw new Error();});
+      it.only('[1]を渡すと0が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([1], mockedDb)).resolves.toBe(0)
+      })
+      it('[1,2,3]を渡すと0が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([1,2,3], mockedDb)).resolves.toBe(0)
+      })
+      it('[]を渡すと0が返却される', async() => {
+        expect(func.asyncSumOfArraySometimesZero([], mockedDb)).resolves.toBe(0)
+      })
+    })
+  })
+  describe('getFirstNameThrowIfLong',() => {
+    const nameApiServiceMock = new NameApiService();
+    const errorMessage = 'first_name too long';
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    })
+    describe('getFirstNameがtaro(4字)を返す', () => {
+      jest.spyOn(nameApiServiceMock, 'getFirstName').mockImplementation(() => Promise.resolve('taro'));
+      it('最大値5字を渡すとtaroが返却される', () => {
+        expect(func.getFirstNameThrowIfLong(5, nameApiServiceMock)).resolves.toBe('taro');
+      })
+      it('最大値4字を渡すとtaroが返却される', () => {
+        expect(func.getFirstNameThrowIfLong(4, nameApiServiceMock)).resolves.toBe('taro');
+      })
+      it('最大値3字を渡すとtaroが返却される', () => {
+        expect(func.getFirstNameThrowIfLong(3, nameApiServiceMock)).rejects.toThrow('first_name too long');
+      })
+    })
+    describe('getFirstNameが例外をthrowする', () => {
+      const errorMessage = 'firstName is too long!'
+      jest.spyOn(nameApiServiceMock, 'getFirstName').mockImplementation(() => { throw new Error(errorMessage); });
+      it('最大値5字を渡すと例外が返却される', () => {
+        expect(func.getFirstNameThrowIfLong(5, nameApiServiceMock)).rejects.toThrow(errorMessage);
+      })
+      it('最大値4字を渡すと例外が返却される', () => {
+        expect(func.getFirstNameThrowIfLong(4, nameApiServiceMock)).rejects.toThrow(errorMessage);
+      })
+      it('最大値3字を渡すと例外が返却される', () => {
+        expect(func.getFirstNameThrowIfLong(3, nameApiServiceMock)).rejects.toThrow(errorMessage);
+      })
+    })
+  })
+  
 })
 
-test('asyncSumOfArrayが正しい計算結果を返す', () => {
-  func.asyncSumOfArray([1,2,3]).then((res) => expect(res).toBe(6))
-})
